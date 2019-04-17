a4f83bbb43ba8ed60230fbe62e8ebe206fa1ffc3
Rahul Kumar <rahul151995@gmail.com>
2019-04-17 15:59:25 +0530

    implemented carrousel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './resources/styles.css';
 import Header from './components/header_footer/header';
+import Featured from './components/feature/index';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" style =  {{height:"1500px", background:'cornflowerblue '}}>
        <Header/>
+       <Featured/>
       </div>
     );
   }
